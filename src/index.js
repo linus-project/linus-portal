a3a92362c3abcf5dc1db749e225de87f1b8f24b4
antoniorslima <antonio.lima@bandtec.com.br>
2022-09-28 21:38:03 -0300

    juntando index com componente de nivel

--- src/index.js
+++ src/index.js
@@ -2,6 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/main.css';
 import Home from './pages/Home';
+import './styles/bootstrap.min.css';
+import './styles/config.css';
+import './styles/global.css';
+import './styles/custom.css';
+import './styles/responsive.css'
+
+// @import url(./bootstrap.min.css);
+// @import url(./config.css);
+// @import url(./global.css);
+// @import url(./custom.css);
+// @import url(./responsive.css);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(

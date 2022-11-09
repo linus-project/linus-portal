e649138f4e2f402d84d9fd0dfc165f750b20cd42
imayaramota <mayara.souza@bandtec.com.br>
2022-11-09 20:24:40 -0300

    fix: ajuste por nível

--- src/pages/Distribuicao.jsx
+++ src/pages/Distribuicao.jsx
@@ -12,6 +12,40 @@ export function Distribuicao() {
         <CTextoGrande text="Distribuições" class="fs-1 pl-5 fw-bold pt-5" />
         <CTextoPequeno text="Selecione a distribuição e veja os planos de estudos disponíveis para você em cada uma delas:" class="pl-5 fw-lighter mb-20"/>
         
+        <CTextoGrande text="Iniciante" class="pl-5 fw-bold pt-5" color="#52BCBF"/>
+        <div className="container text-left">
+            <div className="row">
+                <div className="col">
+                    <CDistribuicao image="../assets/suse.svg" titulo="Distribuição 1" texto="Descrição sobre a distribuição"/>
+                </div>
+                <div className="col">
+                    <CDistribuicao image="../assets/mint.svg" titulo="Distribuição 2" texto="Descrição sobre a distribuição"/>
+                </div>
+                <div className="col">
+                    <CDistribuicao image="../assets/debian.svg" titulo="Distribuição 3" texto="Descrição sobre a distribuição"/>
+                </div>
+                <div className="col">
+                    <CDistribuicao image="../assets/debian.svg" titulo="Distribuição 3" texto="Descrição sobre a distribuição"/>
+                </div>
+            </div>
+        </div>
+
+        <CTextoGrande text="Intermediário" class="pl-5 fw-bold pt-5" color="#52BCBF"/>
+        <div className="container text-left">
+            <div className="row">
+                <div className="col">
+                    <CDistribuicao image="../assets/suse.svg" titulo="Distribuição 1" texto="Descrição sobre a distribuição"/>
+                </div>
+                <div className="col">
+                    <CDistribuicao image="../assets/mint.svg" titulo="Distribuição 2" texto="Descrição sobre a distribuição"/>
+                </div>
+                <div className="col">
+                    <CDistribuicao image="../assets/debian.svg" titulo="Distribuição 3" texto="Descrição sobre a distribuição"/>
+                </div>
+            </div>
+        </div>
+
+        <CTextoGrande text="Avançado" class="pl-5 fw-bold pt-5" color="#52BCBF"/>
         <div className="container text-left">
             <div className="row">
                 <div className="col">
